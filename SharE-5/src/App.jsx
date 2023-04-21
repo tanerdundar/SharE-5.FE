70846513de13b14092396a28f4e7900a3cd8afea
tanerdundar <t.dundar145@gmail.com>
2023-04-21 10:50:31 +0300

    add post.jsx, modify flows

--- SharE-5/src/App.jsx
+++ SharE-5/src/App.jsx
@@ -3,35 +3,18 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Profile from "./components/Profile";
 import Search from "./components/Search";
-import FlowOfSearch from "./components/FlowOfSearch";
 import Flow from "./components/Flow";
 import FlowOfProfile from "./components/FlowOfProfile";
 
 function App() {
-  const [flow, setFlow] = useState(true);
-  const [whichFlow, setWhichFlow] = useState(true);
-  const clickToSearch = () => {
-    setFlow(false);
-    setWhichFlow(false);
-  };
-  const clickToProfile = () => {
-    setFlow(false);
-    setWhichFlow(true);
-  };
-  const clickToRefresh = () => {
-    setFlow(true);
-    setWhichFlow(true);
-  };
+  const [isFlow, setIsFlow] = useState(true);
 
   return (
     <div className="App">
       <NavBar />
       <div className="main">
-        <button onClick={clickToRefresh}>Refresh</button>
-        <button onClick={clickToProfile}>toPrfile</button>
-        <button onClick={clickToSearch}>to Search</button>
         <Profile />
-        {flow ? <Flow /> : whichFlow ? <FlowOfProfile /> : <FlowOfSearch />}
+        {isFlow ? <Flow /> : <FlowOfProfile />}
         <Search />
       </div>
     </div>
--- SharE-5/src/components/Search.jsx
+++ SharE-5/src/components/Search.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import Profile from "./Profile";
+
 function Search() {
+  const [statu, setStatu] = useState(true);
+  const statuHandler = () => {
+    setStatu(false);
+  };
   return (
-    <div id="search" className="search">
-      <input type="text" />
-    </div>
+    <>
+      {statu ? (
+        <div id="search" className="search">
+          <div className="search-inside">
+            <input className="inputer" type="text" placeholder="text here..." />
+            <button onClick={statuHandler} className="get-button">
+              Search
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Profile />
+      )}
+    </>
   );
 }
 
